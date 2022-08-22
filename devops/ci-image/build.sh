761d04ac047c781edb6e3f12867f15d96f9fb7de
pko <pko>
2022-08-22 16:34:34 +0200

    Initial setup #4

--- devops/ci-image/build.sh
+++ devops/ci-image/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-#Tested with version 4.3.1
 set -e
-#pip3 install awscli
-docker build -t qkon/master-ci:latest .
-#docker login -u $DOCKERHUB_LOGIN -p $DOCKERHUB_PASS
-#docker push qkon/master-ci:latest
+docker build -t registry.gitlab.com/qak87/qparking:latest .
+if [ "$1" == "--publish" ]; then
+  docker login registry.gitlab.com
+  docker push registry.gitlab.com/qak87/qparking:latest
+fi
