30d9480b8682273764823c4741e81ac4f2da27ba
pko <pko>
2022-08-23 11:03:34 +0200

    #2 Building python

---
+++ user-service/src/main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+
+
+def us_entrypoint(event: dict, context):
+    print(f"Input event: {str(event)}")
---
+++ user-service/src/tests/test_dummy.py
@@ -0,0 +1,7 @@
+from unittest import TestCase
+
+
+class TestDummy(TestCase):
+    @staticmethod
+    def test_receiving_driver_status() -> None:
+        assert 1 < 2
